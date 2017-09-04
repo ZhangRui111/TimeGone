e2f39077518d9409059ebc7e038abdbdd5106107
ZhangRui111 <zhangruisg111@163.com>
2017-09-04 19:38:24 +0800

    FloatActionMenu

--- app/src/main/java/com/example/john/timegone/Activity/GuideActivity.java
+++ app/src/main/java/com/example/john/timegone/Activity/GuideActivity.java
@@ -4,12 +4,17 @@ import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
@@ -17,6 +22,7 @@ import android.widget.Toast;
 
 import com.example.john.timegone.MainActivity;
 import com.example.john.timegone.R;
+import com.example.john.timegone.Tools.Utils;
 
 import java.util.Calendar;
 
@@ -47,6 +53,7 @@ public class GuideActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        setImmersiveMode();
 
         initData();
         initView();
@@ -276,4 +283,37 @@ public class GuideActivity extends AppCompatActivity implements View.OnClickList
         datePickerDialog.show();
     }
 
+
+    /**
+     * 对于不同版本的安卓系统实现沉浸式体验
+     */
+    private void setImmersiveMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //5.0 全透明实现
+            //getWindow.setStatusBarColor(Color.TRANSPARENT)
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            //调用静态方法设置沉浸式状态栏
+            Utils.setStateBarColor(this);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            //4.4 全透明状态栏
+            //底部导航栏透明
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            //顶部状态栏透明
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+            ViewGroup contentLayout = (ViewGroup) this.findViewById(android.R.id.content);
+            View contentChild = contentLayout.getChildAt(0);
+            contentChild.setFitsSystemWindows(false);  // 这里设置成侵占状态栏，反而不会侵占状态栏，太坑
+            Utils.setupStatusBarView(this, contentLayout, Color.parseColor("#4A90E2"));
+            //隐藏底部导航栏
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            //int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;  此时将同时隐藏状态栏
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
 }
--- app/src/main/java/com/example/john/timegone/Tools/Utils.java
+++ app/src/main/java/com/example/john/timegone/Tools/Utils.java
@@ -1,7 +1,11 @@
 package com.example.john.timegone.Tools;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
 
 import static android.content.Context.MODE_PRIVATE;
 import static com.example.john.timegone.Data.FactorForLifespan.birthDay;
@@ -17,7 +21,10 @@ import static com.example.john.timegone.Data.FactorForLifespan.provinceCode;
 
 public class Utils {
 
-
+    /**
+     * 读取 lifeSpan Factor
+     * @param context
+     */
     public static void funcReadFactorFromSharePre(Context context) {
         SharedPreferences pref = context.getSharedPreferences("user_lifespan_factor",MODE_PRIVATE);
         countryCode = pref.getInt("countryCode", -1);
@@ -26,5 +33,29 @@ public class Utils {
         birthYear = pref.getInt("birthYear", -1);
         birthMonth = pref.getInt("birthMonth", -1);
         birthDay = pref.getInt("birthDay", -1);
+    }
+
+    //状态栏沉浸相关
+    public static void setStateBarColor(Activity activity) {
+        // 设置状态栏颜色
+        ViewGroup contentLayout = (ViewGroup) activity.findViewById(android.R.id.content);
+        setupStatusBarView(activity, contentLayout, Color.parseColor("#4A90E2"));
+        // 设置Activity layout的fitsSystemWindows
+        View contentChild = contentLayout.getChildAt(0);
+        contentChild.setFitsSystemWindows(true);//等同于在根布局设置android:fitsSystemWindows="true"
+    }
+
+    public static void setupStatusBarView(Activity activity, ViewGroup contentLayout, int color) {
+        View mStatusBarView = null;
+        View statusBarView = new View(activity);
+        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getStatusBarHeight(activity));
+        contentLayout.addView(statusBarView, lp);
+        mStatusBarView = statusBarView;
+        mStatusBarView.setBackgroundColor(color);
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int resourceId =context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        return context.getResources().getDimensionPixelSize(resourceId);
     }
 }
